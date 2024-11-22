3c7bd350c474169efa75293262716d4d45dd2059
kylacochrane <Kyla.Cochrane@phac-aspc.gc.ca>
2024-11-22 13:04:01 -0500

    Update test_GAS_assign.py to align with recent updates

--- tests/test_GAS_assign.py
+++ tests/test_GAS_assign.py
@@ -7,10 +7,11 @@ from genomic_address_service.classes.assign import assign
 @pytest.fixture
 def mock_dist_file():
     content = """query_id\tref_id\tdist
+q1\tq1\t0.0
 q1\tr1\t0.1
-q2\tr2\t0.2
+q1\tr2\t0.2
 """
-    with NamedTemporaryFile('w+', delete=False) as tmp:
+    with NamedTemporaryFile('w+', suffix='.tsv', delete=False) as tmp:
         tmp.write(content)
         tmp.flush()
         yield tmp.name
@@ -18,27 +19,25 @@ q2\tr2\t0.2
 
 @pytest.fixture
 def mock_membership_file():
-    content = """id\tthreshold_0.1\tthreshold_0.2
-r1\t1\t1
-r2\t2\t1
+    content = """id\taddress_levels_notsplit
+r1\t1.1
+r2\t2.1
 """
-    with NamedTemporaryFile('w+', delete=False) as tmp:
+    with NamedTemporaryFile('w+', suffix='.tsv', delete=False) as tmp:
         tmp.write(content)
         tmp.flush()
         yield tmp.name
         os.unlink(tmp.name)
 
 def test_initialization(mock_dist_file, mock_membership_file):
-    threshold_map = {"threshold_0.1": 0.1, "threshold_0.2": 0.2}
-    a = assign(dist_file=mock_dist_file, membership_file=mock_membership_file, threshold_map=threshold_map, linkage_method='single')
-    
+    threshold_map = {"level_0": 0.1, "level_1": 0.2}
+    a = assign(dist_file=mock_dist_file, membership_file=mock_membership_file, threshold_map=threshold_map, linkage_method='single', sample_col='id', address_col='address_levels_notsplit', batch_size=100)
     assert a.status, "Initialization failed, check error_msgs for details"
     assert not a.error_msgs, f"Unexpected errors during initialization: {a.error_msgs}"
-    assert isinstance(a.query_df, pd.DataFrame), "Query DataFrame not initialized properly"
     assert isinstance(a.memberships_df, pd.DataFrame), "Memberships DataFrame"
 
 def test_check_membership_columns(mock_dist_file, mock_membership_file):
-    threshold_map = {"threshold_0.1": 0.1, "threshold_0.2": 0.2}
-    a = assign(dist_file=mock_dist_file, membership_file=mock_membership_file, threshold_map=threshold_map, linkage_method='single')
-    cols = ['threshold_0.1', 'threshold_0.2']
+    threshold_map = {"level_0": 0.1, "level_1": 0.2}
+    a = assign(dist_file=mock_dist_file, membership_file=mock_membership_file, threshold_map=threshold_map, linkage_method='single', sample_col='id', address_col='address_levels_notsplit', batch_size=100)
+    cols = ['level_0', 'level_1']
     assert a.check_membership_columns(cols), "Membership column check failed for valid columns"
